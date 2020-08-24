3ea583a8ad533588317ea9809ec9ae386895e562
Chauncey <se7en.s7n@gmail.com>
2020-08-24 13:23:33 -0400

    added a missing slash to the js file of all things; should run now

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs')
 
-fs.readFile(__dirname + 'info.json', 'utf8', (err,data) => {
+fs.readFile(__dirname + '/info.json', 'utf8', (err,data) => {
     if (err){
         console.log(err)
     } else {
